1c258cd2491cdc2982994425ad5ae9f5517fa62c
Silakiniaina <153703946+Silakiniaina@users.noreply.github.com>
2024-03-16 13:21:21 +0300

    Get and Set for Terrain

--- src/display/Terrain.java
+++ src/display/Terrain.java
@@ -8,4 +8,18 @@ import graphe.Sommet;
 public class Terrain extends JPanel{
     Sommet[] listSommet; 
     Mobile[] listMobile;
+
+    
+    public Sommet[] getListSommet() {
+        return listSommet;
+    }
+    public void setListSommet(Sommet[] listSommet) {
+        this.listSommet = listSommet;
+    }
+    public Mobile[] getListMobile() {
+        return listMobile;
+    }
+    public void setListMobile(Mobile[] listMobile) {
+        this.listMobile = listMobile;
+    }
 }
