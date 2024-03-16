1ba6f20e9f39f66d132d7d49e976ef9543af1759
Silakiniaina <153703946+Silakiniaina@users.noreply.github.com>
2024-03-16 13:10:37 +0300

    Get and Set for Sommet

--- src/graphe/Sommet.java
+++ src/graphe/Sommet.java
@@ -4,4 +4,16 @@ import java.awt.Point;
 public class Sommet {
     Sommet[] voisins;
     Point position;
+    public Sommet[] getVoisins() {
+        return voisins;
+    }
+    public void setVoisins(Sommet[] voisins) {
+        this.voisins = voisins;
+    }
+    public Point getPosition() {
+        return position;
+    }
+    public void setPosition(Point position) {
+        this.position = position;
+    }
 }
