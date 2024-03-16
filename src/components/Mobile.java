44059222a4bfe27ac30bce15eecd0d91987b73f0
Silakiniaina <153703946+Silakiniaina@users.noreply.github.com>
2024-03-16 13:16:48 +0300

    Get and Set pour Mobile

--- src/components/Mobile.java
+++ src/components/Mobile.java
@@ -5,4 +5,12 @@ import graphe.Sommet;
 
 public class Mobile extends JPanel{
     Sommet sommet;
+
+    public Sommet getSommet() {
+        return sommet;
+    }
+
+    public void setSommet(Sommet sommet) {
+        this.sommet = sommet;
+    }
 }
